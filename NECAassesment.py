49d563e4bac0e4f3da64869ac5be6ff6ca51580f
Junu0201 <yeoj@wsc.school.nz>
2025-06-23 11:35:08 +1200

    Assesment

---
+++ NECAassesment.py
@@ -0,0 +1,13 @@
+print("Welcome to my rock scissor paper game!")
+score = 0
+
+import random
+user_choice = input("Choose one! rock scissor paper!")
+user_choice = ["rock","scissor","paper"]
+computer_choice = print(random.choice)
+print(random.choice (user_choice))
+
+if user_choice == computer_choice
+    print("Tie")
+    socre += 0
+if user_choice == "rock"
