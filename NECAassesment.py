f2ff0d52f2e0cd846e93017f66a7646ed975e801
Junu0201 <yeoj@wsc.school.nz>
2025-06-24 15:07:08 +1200

    work

--- NECAassesment.py
+++ NECAassesment.py
@@ -1,13 +1,20 @@
-print("Welcome to my rock scissor paper game!")
+print("Welcome to my rock paper scissor game!")
 score = 0
 
-import random
-user_choice = input("Choose one! rock scissor paper!")
-user_choice = ["rock","scissor","paper"]
-computer_choice = print(random.choice)
-print(random.choice (user_choice))
 
-if user_choice == computer_choice
+import random
+user = input("Choose one! rock paper scissor!")
+computer = random.choice(["rock", "paper", "scissor"])
+print ("You chose: " + user)
+print ("Computer chose: " + computer)
+
+
+if user == computer:
     print("Tie")
-    socre += 0
-if user_choice == "rock"
+    score += 0
+if user == "rock" and computer == "paper" or user == "paper" and computer == "scissor" or user == "scissor" and computer == "rock":
+    print("Lose")
+    score -= 1
+if user == "scissor" and computer == "paper" or user == "paper" and computer =="rock" or user == "rock" and computer == "scisssor":
+    print("Win")
+    score += 1
---
+++ pythontest.py
@@ -0,0 +1,7 @@
+import time
+
+print("Timer started")
+
+time.sleep(6)
+
+print("Time's up!")
